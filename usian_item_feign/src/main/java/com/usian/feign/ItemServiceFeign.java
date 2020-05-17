1988f94cf5a952fde7ab4d11a953c7c67b943a95
qaz1123 <1344088914@qq.com>
2020-05-17 14:39:24 +0800

    商品类目查询

--- usian_item_feign/src/main/java/com/usian/feign/ItemServiceFeign.java
+++ usian_item_feign/src/main/java/com/usian/feign/ItemServiceFeign.java
@@ -1,11 +1,14 @@
 package com.usian.feign;
 
 import com.usian.pojo.TbItem;
+import com.usian.pojo.TbItemCat;
 import com.usian.utis.PageResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @FeignClient(value = "usian-item-service")
 public interface ItemServiceFeign {
@@ -17,4 +20,7 @@ public interface ItemServiceFeign {
     @RequestMapping("/service/item/selectTbItemAllByPage")
     public PageResult selectTbItemAllByPage(@RequestParam("page") Integer page,
                                             @RequestParam("rows") Long rows);
+
+    @RequestMapping("/service/itemCat/selectItemCategoryByParentId")
+    List<TbItemCat> selectItemCategoryByParentId(@RequestParam Long id);
 }
